6f016fb84e997f38f0ea9b246c772e7a84a655ce
radiish <thesethskigamer@gmail.com>
2022-11-06 17:13:48 +0000

    initial attribute+modifier impl

---
+++ examples/health_max_health.rs
@@ -0,0 +1,200 @@
+use std::num::NonZeroUsize;
+
+use bevy::{
+    log::{Level, LogPlugin},
+    prelude::*,
+    time::FixedTimestep,
+};
+
+use bevy_attr::{Attribute, AttributePlugin, Modifier, ModifierPlugin};
+
+#[derive(Component, Deref, DerefMut, Default)]
+struct Health(usize);
+impl Attribute for Health {}
+
+#[derive(Component, Deref, DerefMut)]
+struct MaxHealth(usize);
+impl Attribute for MaxHealth {}
+
+impl Default for MaxHealth {
+    fn default() -> Self {
+        MaxHealth(20)
+    }
+}
+
+#[derive(Component, Default)]
+struct ExtraMaxHealthCharm;
+
+impl Modifier for ExtraMaxHealthCharm {
+    type Attr = MaxHealth;
+
+    fn apply(&self, max_health: &mut MaxHealth) {
+        **max_health += 10;
+    }
+}
+
+// since `Health` and `MaxHealth` will both be inserted onto the same entity,
+// we can make `MaxHealth` a modifier for `Health`
+// instead of implementing redundant code on the `Reset` impl for `Health`.
+impl Modifier for MaxHealth {
+    type Attr = Health;
+
+    fn priority(&self) -> isize {
+        // first modifer
+        0
+    }
+
+    fn apply(&self, health: &mut Health) {
+        **health += **self
+    }
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct Damage(usize);
+
+impl Modifier for Damage {
+    type Attr = Health;
+
+    fn priority(&self) -> isize {
+        // after `MaxHealth` in order.
+        1
+    }
+
+    fn apply(&self, health: &mut Health) {
+        // set to zero if would be negative.
+        **health = health.saturating_sub(**self);
+    }
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct RegenRate(usize);
+
+#[derive(Component)]
+struct Actor {
+    pub name: &'static str,
+}
+
+// take damage event.
+struct Hit {
+    actor: Entity,
+    damage: usize,
+}
+
+fn take_damage(
+    mut damaged: Query<(&Actor, &mut Damage)>,
+    undamaged: Query<(Entity, &Actor), Without<Damage>>,
+    mut hits: EventReader<Hit>,
+    mut commands: Commands,
+) {
+    for hit in hits.iter() {
+        if let Ok((actor, mut damage)) = damaged.get_mut(hit.actor) {
+            info!("ouch! {} just took {} more damage!", actor.name, hit.damage);
+            **damage += hit.damage;
+        } else if let Ok((entity, actor)) = undamaged.get(hit.actor) {
+            info!("ouch! {} just took {} damage!", actor.name, hit.damage);
+            commands.entity(entity).insert(Damage(hit.damage));
+        }
+    }
+}
+
+fn regenerate(
+    mut damaged: Query<(Entity, &Actor, &mut Damage, &RegenRate)>,
+    mut commands: Commands,
+) {
+    for (entity, actor, mut damage, regen_rate) in damaged.iter_mut() {
+        info!("phew! {} is healing {} damage!", actor.name, **regen_rate);
+        if let Some(nonzero) = NonZeroUsize::new(damage.saturating_sub(**regen_rate)) {
+            **damage = nonzero.into();
+        } else {
+            info!("phew! {} is fully healed!", actor.name);
+            commands.entity(entity).remove::<Damage>();
+        }
+    }
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn((
+        Actor { name: "Mike" },
+        MaxHealth::default(),
+        Health::default(),
+        RegenRate(10),
+    ));
+
+    commands.spawn((
+        Actor { name: "Paul" },
+        MaxHealth::default(),
+        ExtraMaxHealthCharm::default(),
+        Health::default(),
+        RegenRate(2),
+    ));
+}
+
+fn hit_everyone(everyone: Query<(Entity, &Actor)>, mut hits: EventWriter<Hit>) {
+    let batch = everyone.iter().map(|(entity, actor)| {
+        const DAMAGE: usize = 5;
+        info!("oops! hitting {} for {DAMAGE} damage!", actor.name);
+        Hit {
+            actor: entity,
+            damage: DAMAGE,
+        }
+    });
+    hits.send_batch(batch);
+}
+
+fn kill_dying(dying: Query<(Entity, &Actor, &Health)>, mut commands: Commands) {
+    for (entity, actor, health) in dying.iter() {
+        if **health == 0 {
+            info!("ohno! {} has died!", actor.name);
+            commands.entity(entity).despawn();
+        }
+    }
+}
+
+fn log_health(actors: Query<(&Actor, &Health, &MaxHealth)>) {
+    for (actor, health, max_health) in actors.iter() {
+        info!(
+            "info! {} is at {}/{} health",
+            actor.name, **health, **max_health
+        );
+    }
+}
+
+fn main() {
+    let mut app = App::new();
+    app.add_plugins(MinimalPlugins).add_plugin(LogPlugin {
+        level: Level::INFO,
+        ..Default::default()
+    });
+
+    app.add_plugin(AttributePlugin::<MaxHealth>::default())
+        .add_plugin(ModifierPlugin::<ExtraMaxHealthCharm>::default())
+        .add_plugin(AttributePlugin::<Health>::default())
+        .add_plugin(ModifierPlugin::<MaxHealth>::default())
+        .add_plugin(ModifierPlugin::<Damage>::default());
+
+    app.add_event::<Hit>();
+
+    app.add_startup_system(setup);
+    app.add_system(take_damage);
+    app.add_system_to_stage(CoreStage::Last, kill_dying);
+    app.add_system_set(
+        SystemSet::new()
+            .before(take_damage)
+            .with_run_criteria(FixedTimestep::step(2.))
+            .with_system(hit_everyone),
+    );
+    app.add_system_set(
+        SystemSet::new()
+            .before(take_damage)
+            .with_run_criteria(FixedTimestep::step(1.5))
+            .with_system(regenerate),
+    );
+    app.add_system_set(
+        SystemSet::new()
+            .before(kill_dying)
+            .with_run_criteria(FixedTimestep::step(0.25))
+            .with_system(log_health),
+    );
+
+    app.run();
+}
---
+++ src/lib.rs
@@ -0,0 +1,279 @@
+//! Automatic management of highly modular values with minimal overhead.
+//!
+//! Good examples of things to use this crate for are in-game stats, and
+//! systems like health management (both maximum health and health could be implemented as [attributes][Attribute]).
+//!
+//! See the [`Attribute`] trait for a more detailed overview, or view [the examples].
+//!
+//! [the examples]: https://github.com/istanbul-not-constantinople/bevy_attr/tree/main/examples
+
+use std::{cmp::Ordering, marker::PhantomData};
+
+use bevy::prelude::*;
+use bevy_trait_query::RegisterExt;
+
+/// Resets a variable to its default value.
+///
+/// Implemented for all [`T: Default`][Default],
+/// but you can implement the trait manually for control over keeping certain data between resets.
+///
+/// Required for [`Attribute`].
+///
+/// # Examples
+/// ```rust
+/// use bevy_attr::Reset;
+///
+/// struct Accumulator {
+///     value: usize,
+///     next: fn(usize) -> usize,   
+/// }
+///
+/// impl Reset for Accumulator {
+///     fn reset(&mut self) {
+///         self.value = 0;
+///         // we don't want to reset the next fn here though!
+///     }
+/// }
+///
+/// ```
+pub trait Reset {
+    /// Resets a variable to its default value.
+    fn reset(&mut self);
+}
+
+impl<T: Default> Reset for T {
+    fn reset(&mut self) {
+        *self = Default::default();
+    }
+}
+
+/// Marker trait for components which act as attributes.
+///
+/// An attribute has a base value (defined by the [`Reset`] trait)
+/// and a number of [modifiers][Modifier] which are attached to the same entity.
+///
+/// Attribute values are regenerated when one of their modifiers is added, mutated or removed.
+/// When a modifier is changed, the attribute is marked with [`DirtyAttr`] and a system runs,
+/// first [resetting][Reset] the value of the attribute,
+/// and then applying each modifier attached to the same entity,
+/// sorted by their [priority][Modifier::priority].
+///
+/// All attributes should be registered by adding [`AttributePlugin`]s to your app.
+///
+/// See the [`Modifier`] trait for more information on modifiers.
+///
+/// # Examples
+/// ```rust
+/// use bevy::prelude::*;
+/// use bevy_attr::{AttributePlugin, ModifierPlugin, Attribute, Modifier};
+///
+/// // define our attribute component.
+/// #[derive(Component, Deref, DerefMut)]
+/// struct MaxHealth(usize);
+///
+/// // implementing default implements the `Reset` trait
+/// // which is required for `Attribute`.
+/// impl Default for MaxHealth {
+///     fn default() -> Self {
+///         Self(100)
+///     }
+/// }
+///
+/// // implement the marker trait.
+/// impl Attribute for MaxHealth {}
+///
+///
+/// // define out modifier.
+/// #[derive(Component)]
+/// struct ExtraMaxHealth;
+///
+/// impl Modifier for ExtraMaxHealth {
+///     type Attr = MaxHealth;
+///
+///     fn apply(&self, value: &mut MaxHealth) {
+///         **value += 50;
+///     }
+/// }
+///
+/// let mut app = App::new();
+///
+/// // add the relevant plugins to our app.
+/// app.add_plugin(AttributePlugin::<MaxHealth>::default());
+/// app.add_plugin(ModifierPlugin::<ExtraMaxHealth>::default());
+///
+/// let id = app
+///     .world
+///     .spawn((MaxHealth::default(), ExtraMaxHealth))
+///     .id();
+///
+/// app.update();
+/// // during this update:
+/// // 1. In `CoreStage::Update`, the `ModifierPlugin` notices that the `ExtraMaxHealth` modifier was added
+/// //    to an entity with the `MaxHealth` attribute and gives the entity the `DirtyAttr<MaxHealth>` component.
+/// // 2. In `CoreStage::PostUpdate`, the `AttributePlugin` notices that the `DirtyAttr` component was added
+/// //    and recalculates the attribute. First it resets the attribute value to `MaxHealth(100)`,
+/// //    and then it adds the health from the `ExtraMaxHealth` modifier (a total of 150).
+/// //    The `DirtyAttr` component is then removed.
+///
+/// {
+///     let mut entity = app.world.get_entity_mut(id).unwrap();
+///     let max_health = entity.get::<MaxHealth>().unwrap();
+///     assert_eq!(**max_health, 150);
+///     entity.remove::<ExtraMaxHealth>();
+/// }
+///
+/// app.update();
+///
+/// {
+///     let max_health = app.world.get::<MaxHealth>(id).unwrap();
+///     assert_eq!(**max_health, 100);
+/// }
+/// ```
+pub trait Attribute: Component + Reset {}
+
+/// A modifier on an [`Attribute`].
+///
+/// Modifiers should always be components.
+///
+/// All modifiers should be registered by adding a [`ModifierPlugin`] to your app.
+///
+/// See the [`Attribute`] trait for a more detailed overview.
+#[bevy_trait_query::queryable]
+pub trait Modifier: Send + Sync + 'static {
+    /// The attribute that this modifier modifies.
+    type Attr: Attribute;
+
+    /// Returns the signed priority of the modifier.
+    ///
+    /// This method should be implemented for most modifiers
+    /// since most sequences of operations are order-dependent.
+    ///
+    /// By default the priority is zero.
+    fn priority(&self) -> isize {
+        0
+    }
+
+    /// Applies the modifier to an instance of its associated attribute.
+    fn apply(&self, attr: &mut Self::Attr);
+}
+
+trait ModifierExt: Modifier {
+    #[cfg(debug_assertions)]
+    fn type_name(&self) -> &'static str {
+        std::any::type_name::<Self>()
+    }
+}
+impl<M: Modifier + ?Sized + 'static> ModifierExt for M {}
+
+/// Registers the required information for an [`Attribute`].
+///
+/// The relevant [`ModifierPlugin`]s should also be added to your app.
+#[derive(Default)]
+pub struct AttributePlugin<A: Attribute>(PhantomData<A>);
+
+fn refresh_dirty_attr<A: Attribute>(
+    mut attrs: Query<(Entity, &mut A, Option<&dyn Modifier<Attr = A>>), With<DirtyAttr<A>>>,
+    mut commands: Commands,
+) {
+    for (dirty, mut attr, mods) in attrs.iter_mut() {
+        let mut mods: Vec<_> = mods.map_or_else(Vec::new, |mods| mods.iter().collect());
+        mods.sort_unstable_by(|a, b| {
+            let order = a.priority().cmp(&b.priority());
+            #[cfg(debug_assertions)]
+            if let Ordering::Equal = order {
+                warn!(
+                    "ambiguity between the order of two modifiers ({} and {} have the same priority)",
+                    a.type_name(),
+                    b.type_name(),
+                );
+            }
+            order
+        });
+
+        Reset::reset(&mut *attr);
+
+        for modifier in mods.iter() {
+            modifier.apply(&mut attr);
+        }
+
+        commands.get_entity(dirty).unwrap().remove::<DirtyAttr<A>>();
+    }
+}
+
+impl<A: Attribute> Plugin for AttributePlugin<A> {
+    fn build(&self, app: &mut App) {
+        /// necessary without [bevy-trait-query#11]
+        ///
+        /// [bevy-trait-query#11]: https://github.com/JoJoJet/bevy-trait-query/pull/11
+        #[derive(Component)]
+        struct TraitQueryWorkaround<B: Attribute>(PhantomData<B>);
+        impl<B: Attribute> Modifier for TraitQueryWorkaround<B> {
+            type Attr = B;
+
+            fn apply(&self, _: &mut B) {}
+        }
+        // app.register_component_as::<dyn ModifierQueryable<A>, TraitQueryWorkaround<A>>();
+        app.add_system_to_stage(CoreStage::PostUpdate, refresh_dirty_attr::<A>);
+    }
+}
+
+/// Registers the required information for a [`Modifier`].
+///
+/// The relevant [`AttributePlugin`] should also be added to your app.
+pub struct ModifierPlugin<M: Modifier>(PhantomData<M>);
+
+impl<M: Modifier> Default for ModifierPlugin<M> {
+    fn default() -> Self {
+        Self(PhantomData)
+    }
+}
+
+/// Marker component to indicates that an [`Attribute`]'s modifiers have changed since the last update.
+#[derive(Component)]
+pub struct DirtyAttr<A: Attribute>(PhantomData<A>);
+
+impl<A: Attribute> Default for DirtyAttr<A> {
+    fn default() -> Self {
+        Self(PhantomData)
+    }
+}
+
+fn modifier_changed<M: Modifier + Component>(
+    changed: Query<Entity, (Changed<M>, Without<DirtyAttr<M::Attr>>)>,
+    mut commands: Commands,
+) {
+    for entity in &changed {
+        let mut commands = commands.entity(entity);
+        commands.insert(DirtyAttr::<M::Attr>::default());
+    }
+}
+
+fn modifier_removed<M: Modifier + Component>(
+    removed: RemovedComponents<M>,
+
+    mut commands: Commands,
+) {
+    for entity in &removed {
+        info!("{} removed from {:?}", std::any::type_name::<M>(), entity);
+        let Some(mut commands) = commands.get_entity(entity) else {
+            continue;
+        };
+        commands.insert(DirtyAttr::<M::Attr>::default());
+    }
+}
+
+impl<M: Modifier + Component> Plugin for ModifierPlugin<M> {
+    fn build(&self, app: &mut App) {
+        app.add_system_set_to_stage(
+            CoreStage::PostUpdate,
+            SystemSet::new()
+                .before(refresh_dirty_attr::<M::Attr>)
+                .with_system(modifier_changed::<M>)
+                .with_system(modifier_removed::<M>),
+        );
+        app.register_component_as::<dyn Modifier<Attr = M::Attr>, M>();
+    }
+}
+
+#[cfg(test)]
+mod tests {}
